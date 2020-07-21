e8f5aed8f3acdedcd3753b5d79ea6865993ded27
Asherb123 <46113623+Asherb123@users.noreply.github.com>
2020-07-20 18:24:28 -0700

    yeyeyeeyyeey

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,9 +1,35 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
 public static void main(String[] args) {
 	
+LeagueInvaders c = new LeagueInvaders();
+	c.setup();
+}
+JFrame frame;
+public static final int WIDTH = 500;
+public static final int HIEGHT  = 800;
+
+public LeagueInvaders(){
 	
-	
+	frame= new JFrame();
 	
 }
+
+void setup() {
+	frame.setSize(WIDTH, HIEGHT);
+	
+	
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 }
+
+
+
+
+
+}
+
+
+
+
