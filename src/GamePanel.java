ca299b360e53eb2eb531cca5d236ebe898503aca
Asherb123 <46113623+Asherb123@users.noreply.github.com>
2020-08-03 18:30:20 -0700

    yeyyeye

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,13 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener {
 	@Override
 	public void paintComponent(Graphics g) {
 		if (currentState == MENU) {
@@ -17,17 +20,32 @@ public class GamePanel extends JPanel {
 
 	}
 
+	
+	
+	
+		
+		
+		
+	
+	  
+		
+	
+	Timer frameDraw; 
 	Font fontTitle;
 	Font titleFont;
 
 	public GamePanel() {
-
-		titleFont = new Font("Arial", Font.PLAIN, 48);
+		frameDraw = new Timer(1000/60, this);
+		frameDraw.start();
+		
+	    titleFont = new Font("Arial", Font.PLAIN, 48);
 
 		fontTitle = new Font("Arial", Font.PLAIN, 30);
 
 	}
-
+	
+	
+	
 	final int MENU = 0;
 	final int GAME = 1;
 	final int END = 2;
@@ -73,6 +91,25 @@ public class GamePanel extends JPanel {
 	void drawEndState(Graphics g) {
 		g.setColor(Color.RED);
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HIEGHT);
+		
+		g.setFont(titleFont);
+		g.setColor(Color.WHITE);
+		g.drawString("GAME OVER", 25, 200);
+		g.setColor(Color.white);
+		g.drawString("HAHA! You suck!", 100, 400);
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(currentState == MENU){
+		    updateMenuState();
+		}else if(currentState == GAME){
+		    updateGameState();
+		}else if(currentState == END){
+		    updateEndState();
+		}
+	System.out.println("action");
+	
 	}
 }
